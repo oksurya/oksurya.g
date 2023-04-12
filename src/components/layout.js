db8c41917c313cb6441314e1ebd7c23f808f9041
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2023-04-12 22:19:34 +0530

    ddf

--- src/components/layout.js
+++ src/components/layout.js
@@ -128,7 +128,7 @@ fill="#800099" stroke="none">
 
           <LeftPage/>
       
-          <main className="rounded-lg shadow lg:col-span-9 xl:col-span-6">
+          <main className="lg:pb-5 rounded-lg shadow lg:col-span-9 xl:col-span-6">
 <form className="my-5 mb-10 flex max-w-xl items-center px-6" onSubmit={handleSearch}>
         <input
         className="pl-5 h-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Type your search query here.."
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -26,7 +26,7 @@ const BlogPostTemplate = ({
     
 
       <div class="container mx-auto">
-  <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
+  <div class="max-w-3xl mx-auto px-4 sm:px-6">
     <article>
 
 <header class="border-b mx-auto max-w-2xl text-center">
