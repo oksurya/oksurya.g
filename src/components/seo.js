d42bb684dfd8e24afc0bf8dcb03ea5a4006f08c9
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2023-04-15 02:00:24 +0530

    rt

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,8 +30,8 @@ const Seo = ({ description, title, image, children }) => {
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
 
-  const ogImageUrl = site.siteMetadata.siteUrl + ( image || "/image/?text=" + title );
-  const ogUrl = site.siteMetadata.siteUrl + useLocation().pathname;
+    const ogImageUrl = site.siteMetadata.siteUrl + ( image ||  site.siteMetadata.defaultOpenGraphImage )
+    const ogUrl = site.siteMetadata.siteUrl + useLocation().pathname;
   return (
     <>
       <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
