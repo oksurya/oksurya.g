9f582c194ede615832ee31b23db7d6e111e2153a
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2023-04-16 17:38:27 +0530

    fgf

--- src/pages/tools/converter/image-to-pdf-converter.js
+++ src/pages/tools/converter/image-to-pdf-converter.js
@@ -15,23 +15,62 @@ function ImageToPDFConverter() {
     doc.addImage(image, 'JPEG', 10, 10, 190, 0); // Add the image to the PDF with appropriate dimensions
     doc.save('converted.pdf');
   }
-  
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setImage(URL.createObjectURL(event.dataTransfer.files[0]));
+  };
   return (
   <Layout>
    <header class="mx-auto max-w-2xl text-center">
         <h1 class="text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl">Image to PDF Converter</h1>
         <p class="mt-2 text-sm font-semibold text-gray-400">Turn your images into PDFs effortlessly</p>
     </header>
+
+    
+
+
   <div class="mmax-w-2xl prose mx-auto mt-10 lg:text-lg">
         <div class="relative px-4 sm:px-6 lg:px-3 flex flex-col gap-2">
-    <div>
-      <h1>Image to PDF Converter</h1>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      <button onClick={convertToPDF}>Convert to PDF</button>
-      {image && <img src={image} alt="Selected Image" />}
-    </div>
+
+
+        <div onDragOver={handleDragOver}
+      onDrop={handleDrop}>
+    <label
+        class="lg:h-64 flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+        <span class="flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+            </svg>
+            <span class="font-medium text-gray-600">
+                Drop files to Attach, or
+                <span class="text-purple-600 underline"> browse</span>
+            </span>
+        </span>
+        <input type="file" accept="image/*" onChange={handleImageChange} class="hidden"/>
+    </label>
+</div>
+
+{image && 
+      <>
+      
+      <div id="image-container" className="text-center w-full p-4 border-2 border-dashed border-gray-300">
+          <img src={image} alt="Selected" className="max-w-full h-auto" />
+          <button onClick={convertToPDF} className=" bg-purple-800 text-white py-2 px-4 rounded-lg hover:bg-purple-700 focus:outline-none focus:bg-purple-800">Download PDF</button>
+
+        </div>
+
+
+
+  </>
+      }
     
-    
+  
     </div></div>
      <article className="mt-6 prose prose-indigo px-6 lg:px-8 prose-lg text-gray-500 mx-auto">
 
