d58ea130cc8805c65284357ba7ab70ce69bcd91d
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2023-04-27 18:36:05 +0530

    fg

--- src/pages/tools/converter/image-to-pdf-converter.js
+++ src/pages/tools/converter/image-to-pdf-converter.js
@@ -13,7 +13,7 @@ function ImageToPDFConverter() {
   const convertToPDF = () => {
     const doc = new jsPDF();
     doc.addImage(image, 'JPEG', 10, 10, 190, 0); // Add the image to the PDF with appropriate dimensions
-    doc.save('converted.pdf');
+    doc.save('converted_oksurya.pdf');
   }
   const handleDragOver = (event) => {
     event.preventDefault();
@@ -36,6 +36,23 @@ function ImageToPDFConverter() {
   <div class="mmax-w-2xl prose mx-auto mt-10 lg:text-lg">
         <div class="relative px-4 sm:px-6 lg:px-3 flex flex-col gap-2">
 
+        <div onDragOver={handleDragOver} onDrop={handleDrop}>
+    <label
+        class="lg:h-64 flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+        <span class="flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+            </svg>
+            <span class="font-medium text-gray-600">
+                Drop Image files to Attach, or
+                <span class="text-purple-600 underline"> browse</span>
+            </span>
+        </span>
+        <input type="file" accept="image/*" onChange={handleImageChange} class="hidden"/>
+    </label>
+</div>
 
 
 {image && 
