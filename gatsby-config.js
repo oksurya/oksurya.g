3f741d78b5c39ea1de2f19becbe9a6ac5a7ee7df
Jayasurya Mailsamy <67519756+oksurya@users.noreply.github.com>
2023-05-10 07:07:35 +0530

    dfg

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,55 +97,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.frontmatter.path,
-                  guid: site.siteMetadata.siteUrl + node.frontmatter.path,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `{
-              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
-                nodes {
-                  excerpt
-                  html
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                    date
-                  }
-                }
-              }
-            }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Jayasurya Blog`,
--- src/pages/p/dmca.js
+++ src/pages/p/dmca.js
@@ -87,7 +87,7 @@ function DMCA() {
 
 <h2>Disclaimers for oksurya</h2>
 
-<p>All the information on this website - https://www.oksurya.in - is published in good faith and for general information purpose only. oksurya does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (oksurya), is strictly at your own risk. oksurya will not be liable for any losses and/or damages in connection with the use of our website.</p>
+<p>All the information on this website - https://oksurya.in - is published in good faith and for general information purpose only. oksurya does not make any warranties about the completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website (oksurya), is strictly at your own risk. oksurya will not be liable for any losses and/or damages in connection with the use of our website.</p>
 
 <p>From our website, you can visit other websites by following hyperlinks to such external sites. While we strive to provide only quality links to useful and ethical websites, we have no control over the content and nature of these sites. These links to other websites do not imply a recommendation for all the content found on these sites. Site owners and content may change without notice and may occur before we have the opportunity to remove a link which may have gone 'bad'.</p>
 
